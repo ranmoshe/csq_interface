810d0a26db0df368021e02ba84da0056f5509f83
Ran Moshe <rani.moshe@gmail.com>
2016-06-23 19:01:30 +0300

    some cosmetic changes - doubtful if they are an improvement

--- csq_interface/csq_interface/forms.py
+++ csq_interface/csq_interface/forms.py
@@ -9,3 +9,8 @@ class DataConfigForm(forms.Form):
     configfile = forms.FileField(
         label='Select a config file'
     )
+
+    def __init__(self, *args, **kwargs):
+        super(DataConfigForm, self).__init__(*args, **kwargs)
+        self.fields['datafile'].widget.attrs.update({'class': 'fileUpload btn btn-primary'})
+        self.fields['configfile'].widget.attrs.update({'class': 'fileUpload btn btn-primary'})
